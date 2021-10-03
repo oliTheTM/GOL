7db49c68a87d135eec2589b3cff1c8a9ec6f1385
Oliver Levine <olevine45@gmail.com>
2021-10-03 16:50:00 +0100

    finished UI

--- GOL/MainWindow.xaml.cs
+++ GOL/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Threading;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 
@@ -6,35 +8,139 @@ namespace GOL
 {
     public partial class MainWindow : Window
     {
+        private const int LENGTH = 30;
+
+        private Thread gameThread;
+
         public MainWindow()
         {
+            gameThread = null;
             InitializeComponent();
 
-            //Load grid of cells:
-            for (int i = 0; i < 30; i++) {
-                for (int j = 0; j < 30; j++) {
-                    Button cell = new Button();
+            //Load the game grid:
+            Button cell;
+            for (int i = 0; i < LENGTH; ++i) {
+                for (int j = 0; j < LENGTH; ++j) {
+                    cell = new Button();
                     cell.Background = Brushes.Black;
-                    cell.Name = "cell_"+((15*i) + j).ToString();
+                    cell.Name = "cell_"+((LENGTH * i) + j).ToString();
+                    cell.Click += new RoutedEventHandler(Button_Click);
                     Grid.SetRow(cell, i);
                     Grid.SetColumn(cell, j);
-                    gridMain.Children.Add(cell);
+                    //adds children in same order as in name
+                    GOL.Children.Add(cell);
+                }
+            }
+
+            //finally add start button:
+            Button start = new Button();
+            start.Name = "start";
+            start.Content = "GO!";
+            start.Click += new RoutedEventHandler(Button_Start);
+            Grid.SetRow(start, (LENGTH + 1));
+            Grid.SetColumn(start, (LENGTH/2));
+            GOL.Children.Add(start);
+        }
+
+        /**
+         * Changes state of cell
+         */
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button cell = (Button)sender;
+            if (cell.Background.Equals(Brushes.Black))
+                cell.Background = Brushes.White;
+            else
+                cell.Background = Brushes.Black;
+        }
+
+        /**
+         * 
+         */
+        private void Button_Start(object sender, RoutedEventArgs e)
+        {
+            Button start = (Button)sender;
+            if (start.Content.Equals("GO!")) {
+                start.Content = "STOP";
+                gameThread = new Thread(game);
+                gameThread.Start();
+            }
+            else {
+                start.Content = "GO!";
+                try {
+                    gameThread.Abort();
+                    gameThread.Join();
+                } catch (ThreadAbortException){
+                    ;//Assumption: gameThread halted
                 }
             }
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void game()  {Dispatcher.Invoke(() =>
         {
-            Button cell = (Button)sender;
+            int neighbours;
+            Button cell;
+    
+            while (true) {
+                //forAll <i, j> of the board:
+                for (int i = 0; (i < LENGTH); ++i)
+                {
+                    for (int j = 0; (j < LENGTH); ++j)
+                    {
+                        //reference cell (i, j)
+                        cell = (Button)GOL.Children[(LENGTH * i) + j];
+                        //get neighbour count
+                        neighbours = liveNeighbours(i, j);
+                        //State transition w/r neighbour count:
+                        switch (neighbours)
+                        {
+                            case 0:
+                                cell.Background = Brushes.Black;
+                                break;
+                            case 1:
+                                cell.Background = Brushes.Black;
+                                break;
+                            case 2:
+                                //no change
+                                break;
+                            case 3:
+                                cell.Background = Brushes.White;
+                                break;
+                            case 4:
+                                cell.Background = Brushes.Black;
+                                break;
+                            case 5:
+                                cell.Background = Brushes.Black;
+                                break;
+                            case 6:
+                                cell.Background = Brushes.Black;
+                                break;
+                            case 7:
+                                cell.Background = Brushes.Black;
+                                break;
+                            case 8:
+                                cell.Background = Brushes.Black;
+                                break;
+                        }
+                    }
+                }
+        }});}
 
-            if (cell.Name.Equals("start"))
-            {
-                ;
+        private int liveNeighbours(int x, int y) 
+        {
+            int living = 0, location;
+            for (int Xoff = -1; (Xoff < 2); ++Xoff) {
+                for (int Yoff = -1; (Yoff < 2); ++Yoff) {
+                    location = (x + Xoff) * LENGTH + y + Yoff;
+                    if ((Xoff == 0) && (Yoff == Xoff))
+                        ;//skip if self
+                    else if ((location < 0) || (location > (Math.Pow(LENGTH, 2) - 1)))
+                        ;//skip if outside grid
+                    else if (((Button)GOL.Children[location]).Background.Equals(Brushes.White))
+                        ++living;//add to living if white
+                }
             }
-            else if (cell.Background.Equals(Brushes.Black))
-                cell.Background = Brushes.White;
-            else
-                cell.Background = Brushes.Black;
+            return living;
         }
     }
 }
